8c5a6395c81b03ec5b174310c7e212dce7746343
ajay <ajaytarapara77@gmail.com>
2023-02-10 17:33:30 +0530

    Added CI platform mvc

---
+++ MVC/CI_PLATEFORM/CI_PLATEFORM/Controllers/Account/Account.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CI_PLATEFORM.Controllers.Account
+{
+    public class Account : Controller
+    {
+        public IActionResult login()
+        {
+            return View();
+        }
+        public IActionResult resetpassword()
+        {
+            return View();
+        }
+
+
+    }
+}
