ee6450d2c9e8d906685e6b236175d5ba379e7d18
ajay <ajaytarapara77@gmail.com>
2023-02-15 17:24:05 +0530

    added

--- MVC/CI_PLATEFORM/CI_PLATEFORM/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CI_PLATEFORM.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
