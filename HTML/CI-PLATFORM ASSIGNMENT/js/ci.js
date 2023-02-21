cac4e04254970234c1e050c86672e5e414b8d6c3
ajay <ajaytarapara77@gmail.com>
2023-02-21 18:26:26 +0530

    mission listing

--- HTML/CI-PLATFORM ASSIGNMENT/js/ci.js
+++ HTML/CI-PLATFORM ASSIGNMENT/js/ci.js
@@ -15,3 +15,43 @@ function listview() {
       elements[i].style.width = "32%";
     }
   }
+
+let flag=0;
+function menubartoggle(){ 
+  let p=document.getElementById("menubarimg");
+  let q=document.getElementById("menubar");
+  if(flag%2==0){
+    q.setAttribute("style","display:block");
+  }
+  else{
+    q.setAttribute("style","display:none");
+  }
+  console.log(flag);
+  flag++;
+  
+}
+function closemenu(){
+  let p=document.getElementById("closingimg");
+  let q=document.getElementById("menubar");
+  q.setAttribute("style","display:none");
+}
+
+let flagfilter=0;
+function filterbartoggle(){
+  let p=document.getElementById("closingimg");
+  let q=document.getElementById("filterbar");
+  if(flagfilter%2==0){
+    q.setAttribute("style","display:block");
+  }
+  else{
+    q.setAttribute("style","display:none");
+  }
+  console.log(q);
+  console.log(p);
+  flagfilter++;
+}
+function closefilter(){
+  let p=document.getElementById("closingimg");
+  let q=document.getElementById("filterbar");
+  q.setAttribute("style","display:none");
+}
